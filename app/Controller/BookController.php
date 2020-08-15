bdc43316a86424ac26342036e60f677935c96661
Carlos Gomez <salos_12@hotmail.com>
2020-08-15 15:51:40 -0400

    Book controller methods

---
+++ app/Controller/BookController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Singleton\Container;
+use App\Entity\Book;
+
+class BookController
+{
+    public function getAll() {
+        $allBooks = $this->getBookRepository()->findAll();
+        return response()->httpCode($allBooks ? 200 : 404)->json($allBooks);
+    }
+
+    public function getById($id) {
+        $book = $this->getBookRepository()->find($id);
+        return response()->httpCode($book ? 200 : 404)->json($book);
+    }
+
+    private function getBookRepository()
+    {
+        return Container::getInstance()->getEntityManager()->getRepository(Book::class);
+    }
+
+}
