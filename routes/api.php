88a5e48f9558581c504026fe9470157e1b1d490c
Carlos Gomez <salos_12@hotmail.com>
2020-08-15 16:20:42 -0400

    Page routes and error handling

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,25 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 use App\Router;
+use Pecee\Http\Request;
+use Pecee\SimpleRouter\Exceptions\NotFoundHttpException;
 
 Router::group(['prefix' => '/api'], function () {
     // Book
     Router::get('/book', 'BookController@getAll');
     Router::get('/book/{id}', 'BookController@getById');
+
+    // Page
+    Router::get('/page/{id}', 'PageController@getById');
+});
+
+Router::error(function(Request $request, \Exception $exception) {
+    if($exception instanceof NotFoundHttpException && $exception->getCode() === 404) {
+        response()
+            ->httpCode(404)
+            ->json([
+                'error' => $exception->getMessage(),
+            ]);
+    }
+
 });
