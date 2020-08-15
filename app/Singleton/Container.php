4931314e485567edd0342e86f68013a2743f0f4b
Carlos Gomez <salos_12@hotmail.com>
2020-08-15 15:46:46 -0400

    Making a singleton container to get doctrine entity manager easily

---
+++ app/Singleton/Container.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Singleton;
+
+
+use Doctrine\ORM\EntityManager;
+
+final class Container
+{
+    private static $instance = null;
+
+    private $entityManager;
+
+    /**
+     * @return Container
+     */
+    public static function getInstance()
+    {
+        if (static::$instance === null) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * @param EntityManager $entityManager
+     */
+    public function setEntityManager(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @return EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->entityManager;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -4,15 +4,18 @@ header("Access-Control-Allow-Origin: *");
 header( "Access-Control-Allow-Headers: *");
 
 require '../vendor/autoload.php';
+require '../bootstrap.php';
 
 // Start the routing
-use Src\Router;
+use App\Router;
+use App\Singleton\Container;
 
 /**
  * The default namespace for route-callbacks, so we don't have to specify it each time.
  * Can be overwritten by using the namespace config option on your routes.
  */
-Router::setDefaultNamespace('\Src\Controller');
+Router::setDefaultNamespace('\App\Controller');
+Container::getInstance()->setEntityManager($entityManager);
 
 // Start the routing
 Router::start();
