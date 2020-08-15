d5eab3dcad8f00b612a53db67d9eb7013d2cd1f1
Carlos Gomez <salos_12@hotmail.com>
2020-08-15 16:20:25 -0400

    Basic Page controller

---
+++ app/Controller/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Singleton\Container;
+use App\Entity\Page;
+
+class PageController
+{
+    public function getById($id) {
+        $page = $this->getPageRepository()->find($id);
+        return response()->httpCode($page ? 200 : 404)->json($page ? $page : []);
+    }
+
+    private function getPageRepository()
+    {
+        return Container::getInstance()->getEntityManager()->getRepository(Page::class);
+    }
+
+}
