1b68d806d978a4ede419460cdb0435bbe7676877
Carlos Gomez <salos_12@hotmail.com>
2020-08-16 10:29:48 -0400

    Improving api utils functions

--- app/Utils/ApiUtils.php
+++ app/Utils/ApiUtils.php
@@ -9,16 +9,21 @@ use Doctrine\Common\Collections\Collection;
 
 class ApiUtils
 {
-    public static function getPageApiUrlFormat($bookId, Collection $pages)
+    public static function getPagesApiUrlFormat(Collection $pages)
     {
-        return array_map(function(Page $page) use ($bookId) {
-            $urlParams = ['bookId' => $bookId, 'pageId' => $page->getId()];
-            return $_SERVER['HTTP_HOST'] . url('page_book', $urlParams);
+        return array_map(function(Page $page) {
+            return ApiUtils::getPageApiUrlFormat($page);
         }, $pages->toArray());
     }
 
     public static function getBookApiUrlFormat($bookId)
     {
         return $_SERVER['HTTP_HOST'] . url('book', ['id' => $bookId]);
+    }
+
+    public static function getPageApiUrlFormat(Page $page)
+    {
+        $urlParams = ['bookId' => $page->getBook()->getId(), 'pageId' => $page->getId()];
+        return $_SERVER['HTTP_HOST'] . url('page_book', $urlParams);
     }
 }
