63c1421006206eb736bf63eaa8c5a962e8d8a3fe
Carlos Gomez <salos_12@hotmail.com>
2020-08-16 14:50:16 -0400

    Return an error if format is not available

--- app/Controller/PageController.php
+++ app/Controller/PageController.php
@@ -7,11 +7,6 @@ use App\Service\PageService;
 
 class PageController
 {
-    public function getById($id, $format)
-    {
-        return PageService::getById($id, $format);
-    }
-
     public function getByBookId($bookId, $pageId, $format)
     {
         return PageService::getByBookId($bookId, $pageId, $format);
--- app/Service/PageService.php
+++ app/Service/PageService.php
@@ -10,21 +10,19 @@ use App\Singleton\Container;
 
 class PageService
 {
-    public static function getById($id, $format)
-    {
-        $entityManager = Container::getInstance()->getEntityManager();
-        $page = $entityManager->getRepository(Page::class)->find($id);
-        return response()->httpCode($page ? 200 : 404)->json($page ? $page : []);
-    }
-
     public static function getByBookId($bookId, $pageId, $format)
     {
+        $pageFormat = PageService::getPageFormatByType($pageId, $format);
+        if (!$pageFormat) {
+            return response()->httpCode(404)->json([
+                'error' => 'Format not available',
+            ]);
+        }
         $entityManager = Container::getInstance()->getEntityManager();
         $page = $entityManager->getRepository(Page::class)->findOneBy([
             'book' => $bookId,
             'id' => $pageId,
         ]);
-        $pageFormat = PageService::getPageFormatByType($pageId, $format);
         return response()->httpCode($page ? 200 : 404)->json([
             'page' => $page,
             'format' => $pageFormat
