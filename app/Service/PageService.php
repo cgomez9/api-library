6f1be508a5ba54a27488e6335ccd3fe07f0faa03
Carlos Gomez <salos_12@hotmail.com>
2020-08-16 10:30:54 -0400

    Return page format on page controller

--- app/Service/PageService.php
+++ app/Service/PageService.php
@@ -5,6 +5,7 @@ namespace App\Service;
 
 
 use App\Entity\Page;
+use App\Entity\PageFormat;
 use App\Singleton\Container;
 
 class PageService
@@ -23,6 +24,19 @@ class PageService
             'book' => $bookId,
             'id' => $pageId,
         ]);
-        return response()->httpCode($page ? 200 : 404)->json($page ? $page : []);
+        $pageFormat = PageService::getPageFormatByType($pageId, $format);
+        return response()->httpCode($page ? 200 : 404)->json([
+            'page' => $page,
+            'format' => $pageFormat
+        ]);
+    }
+
+    private static function getPageFormatByType($pageId, $format)
+    {
+        $entityManager = Container::getInstance()->getEntityManager();
+        return $entityManager->getRepository(PageFormat::class)->findOneBy([
+            'page' => $pageId,
+            'type' => $format,
+        ]);
     }
 }
